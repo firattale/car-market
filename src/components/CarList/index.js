94e077e7ba0c265a11c41ebd59bbaf67ccf5162c
Fırat Tale <firattale@hotmail.com>
2020-10-10 11:05:45 +0300

    Create Pagination component

--- src/components/CarList/index.js
+++ src/components/CarList/index.js
@@ -12,6 +12,13 @@ const CarList = () => {
       <h1>Available Cars</h1>
       <p>Showing {totalCarsCount / totalPageCount} of {cars.totalCarsCount} Results</p>
       {cars.cars.map(car => car && <CarCard key={car.stockNumber} car={car} />)}
+      <div className="d-flex justify-content-center mt-3">
+        <span className="pag-button">First</span>
+        <span className="pag-button">Previous</span>
+        <span className="page-count">Page 2 of 10</span>
+        <span className="pag-button">Next</span>
+        <span className="pag-button">Last</span>
+      </div>
     </div>
   );
 };
