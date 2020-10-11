1013c2dbfcd6dc20b180f142ec2ba628ad655929
Fırat Tale <firattale@hotmail.com>
2020-10-11 17:00:04 +0300

    Updates

---
+++ precache-manifest.f0786ddcf36dd9f682047f2824aaa889.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3a13d057bd5c8c08f18b0429acb354",
+    "url": "/auto1/index.html"
+  },
+  {
+    "revision": "fafc7af65d90336dba2b",
+    "url": "/auto1/static/css/main.529d79d9.chunk.css"
+  },
+  {
+    "revision": "9c70734282d813e8a0ab",
+    "url": "/auto1/static/js/2.45f0edf3.chunk.js"
+  },
+  {
+    "revision": "885c0e205ef5e79f4aef448a818ba6a1",
+    "url": "/auto1/static/js/2.45f0edf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fafc7af65d90336dba2b",
+    "url": "/auto1/static/js/main.0a5651f2.chunk.js"
+  },
+  {
+    "revision": "2bcc6adb6ed252d99d2a",
+    "url": "/auto1/static/js/runtime-main.d01650b6.js"
+  },
+  {
+    "revision": "fb20abffd6ef7a76d0fe114e283e1bc3",
+    "url": "/auto1/static/media/logo.fb20abff.png"
+  }
+]);
