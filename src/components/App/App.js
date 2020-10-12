37944f2b938822ff9178a4a7bf0fcfd9f1d879af
Fırat Tale <firattale@hotmail.com>
2020-10-12 09:53:11 +0300

    Code Splitting

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,9 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom';
-import Home from '../../routes/Home/Home';
-import Details from '../../routes/Details/Details';
-import Page404 from '../../routes/Page404/Page404';
 import './App.scss';
 import Header from '../../commons/Header/Header';
 import Footer from '../../commons/Footer/Footer';
@@ -14,25 +11,32 @@ import { useSelector } from 'react-redux';
 import { selectError } from '../../app/carsSlice';
 import AlertComponent from '../../commons/Alert/Alert';
 
+const Details = lazy(() => import('../../routes/Details/Details'));
+const Home = lazy(() => import('../../routes/Home/Home'));
+const Page404 = lazy(() => import('../../routes/Page404/Page404'));
+
 export const App = () => {
   const error = useSelector(selectError);
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="app">
-        <Header/>
-        {error && <div className="d-flex justify-content-center"><AlertComponent variant="danger" error={error}/></div>}
-        <Switch>
-          <Route path="/details/:id">
-            <Details />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="*">
-            <Page404 />
-          </Route>
-        </Switch>
-        <Footer/>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Header />
+          {error && <div className="d-flex justify-content-center"><AlertComponent variant="danger" error={error} /></div>}
+          <Switch>
+            <Route path="/details/:id">
+              <Details />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="*">
+              <Page404 />
+            </Route>
+          </Switch>
+          <Footer />
+        </Suspense>
+
       </div>
     </Router>
   );
