89171d206f4164bea643fba38c8f392493bbf9cc
Fırat Tale <firattale@hotmail.com>
2020-10-12 17:53:47 +0300

    Add helper test

--- src/helpers/helpers.test.js
+++ src/helpers/helpers.test.js
@@ -4,3 +4,7 @@ it('should capitalize first letter', () => {
   const str = 'str';
   expect(capitalize(str)).toBe('Str');
 });
+it('should return "" if it is not string', () => {
+  const str = 4;
+  expect(capitalize(str)).toBe('');
+});
