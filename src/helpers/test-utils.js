9273afbee5f0f33056745664089d57105f48eed5
Fırat Tale <firattale@hotmail.com>
2020-10-23 10:36:23 +0300

    Delete comments

--- src/helpers/test-utils.js
+++ src/helpers/test-utils.js
@@ -1,40 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import carsReducer, { initialState } from '../app/carsSlice';
-
-// const Wrapper = ({ children }) => {
-//   return (
-//     <Provider store={store}>
-//       <MemoryRouter>{children}</MemoryRouter>
-//     </Provider>
-//   );ğ
-// };
-
-// const customRender = (ui, mockState, options) => {
-//   const state = mockState || initialState;
-//   const store = configureStore({
-//     reducer: {
-//       cars: carsReducer
-//     },
-//     preloadedState: { cars: mockState }
-//   });
-//   const Wrapper = ({ children }) => {
-//     return (
-//       <Provider store={store}>
-//         <MemoryRouter>{children}</MemoryRouter>
-//       </Provider>
-//     );
-//   };
-//   return render(ui, { wrapper: Wrapper, ...options });
-
-//   // render(
-//   //   <Provider store={store}>
-//   //     <MemoryRouter>{ui}</MemoryRouter>
-//   //   </Provider>, ...options);
-// };
 
 export const wrappedComponent = (ui, mockState) => {
   const state = mockState || initialState;
@@ -45,13 +13,11 @@ export const wrappedComponent = (ui, mockState) => {
     },
     preloadedState: { cars: state }
   });
-  return ([(<Provider store={store}>
-    <MemoryRouter>{ui}</MemoryRouter>
-  </Provider>), store]
+  return ([(
+    <Provider store={store}>
+      <MemoryRouter>{ui}</MemoryRouter>
+    </Provider>), store]
   );
 };
 // re-export everything
 export * from '@testing-library/react';
-
-// override render method
-// export { customRender as render };
